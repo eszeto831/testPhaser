60d90fd411c0c64e422e8aa870217bbb63f7b7ee
Edmond Szeto <eszeto831@gmail.com>
2016-06-19 14:59:24 -0700

    initial commit

---
+++ www/src/Game.js
@@ -0,0 +1,213 @@
+/* globals Phaser:false */
+// create BasicGame Class
+BasicGame = {
+
+};
+
+// create Game function in BasicGame
+BasicGame.Game = function (game) {
+
+    this.player;
+    this.platforms;
+    this.cursors;
+
+    this.stars;
+    this.score = 0;
+    this.scoreText;
+};
+
+// set Game function prototype
+BasicGame.Game.prototype = {
+
+    init: function () {
+        // set up input max pointers
+        this.input.maxPointers = 1;
+        // set up stage disable visibility change
+        this.stage.disableVisibilityChange = true;
+        // Set up the scaling method used by the ScaleManager
+        // Valid values for scaleMode are:
+        // * EXACT_FIT
+        // * NO_SCALE
+        // * SHOW_ALL
+        // * RESIZE
+        // See http://docs.phaser.io/Phaser.ScaleManager.html for full document
+        this.scale.scaleMode = Phaser.ScaleManager.EXACT_FIT;
+        // If you wish to align your game in the middle of the page then you can
+        // set this value to true. It will place a re-calculated margin-left
+        // pixel value onto the canvas element which is updated on orientation /
+        // resizing events. It doesn't care about any other DOM element that may
+        // be on the page, it literally just sets the margin.
+        this.scale.pageAlignHorizontally = true;
+        this.scale.pageAlignVertically = true;
+        // Force the orientation in landscape or portrait.
+        // * Set first to true to force landscape. 
+        // * Set second to true to force portrait.
+        this.scale.forceOrientation(false, true);
+        // Sets the callback that will be called when the window resize event
+        // occurs, or if set the parent container changes dimensions. Use this 
+        // to handle responsive game layout options. Note that the callback will
+        // only be called if the ScaleManager.scaleMode is set to RESIZE.
+        this.scale.setResizeCallback(this.gameResized, this);
+        // Set screen size automatically based on the scaleMode. This is only
+        // needed if ScaleMode is not set to RESIZE.
+        this.scale.updateLayout(true);
+        // Re-calculate scale mode and update screen size. This only applies if
+        // ScaleMode is not set to RESIZE.
+        this.scale.refresh();
+
+    },
+
+    preload: function () {
+        this.time.advancedTiming = true;
+        this.load.image('sky', 'assets/sky.png');
+        this.load.image('ground', 'assets/platform.png');
+        this.load.image('star', 'assets/star.png');
+        this.load.spritesheet('dude', 'assets/dude.png', 32, 48);
+    },
+
+    create: function () {
+        //  We're going to be using physics, so enable the Arcade Physics system
+        this.physics.startSystem(Phaser.Physics.ARCADE);
+
+        //  A simple background for our game
+        this.add.sprite(0, 0, 'sky');
+
+        //  The platforms group contains the ground and the 2 ledges we can jump on
+        this.platforms = this.add.group();
+
+        //  We will enable physics for any object that is created in this group
+        this.platforms.enableBody = true;
+
+        // Here we create the ground.
+        var ground = this.platforms.create(0, this.world.height - 64, 'ground');
+
+        //  Scale it to fit the width of the game (the original sprite is 400x32 in size)
+        ground.scale.setTo(2, 2);
+
+        //  This stops it from falling away when you jump on it
+        ground.body.immovable = true;
+
+        //  Now let's create two ledges
+        var ledge = this.platforms.create(400, 400, 'ground');
+        ledge.body.immovable = true;
+
+        ledge = this.platforms.create(-150, 250, 'ground');
+        ledge.body.immovable = true;
+
+        // The player and its settings
+        this.player = this.add.sprite(32, this.world.height - 150, 'dude');
+
+        //  We need to enable physics on the player
+        this.physics.arcade.enable(this.player);
+
+        //  Player physics properties. Give the little guy a slight bounce.
+        this.player.body.bounce.y = 0.2;
+        this.player.body.gravity.y = 300;
+        this.player.body.collideWorldBounds = true;
+
+        //  Our two animations, walking left and right.
+        this.player.animations.add('left', [0, 1, 2, 3], 10, true);
+        this.player.animations.add('right', [5, 6, 7, 8], 10, true);
+
+        //  Finally some stars to collect
+        this.stars = this.add.group();
+
+        //  We will enable physics for any star that is created in this group
+        this.stars.enableBody = true;
+
+        //  Here we'll create 12 of them evenly spaced apart
+        for (var i = 0; i < 12; i++)
+        {
+            //  Create a star inside of the 'stars' group
+            var star = this.stars.create(i * 70, 0, 'star');
+
+            //  Let gravity do its thing
+            star.body.gravity.y = 300;
+
+            //  This just gives each star a slightly random bounce value
+            star.body.bounce.y = 0.7 + Math.random() * 0.2;
+        }
+
+        //  The score
+        this.scoreText = this.add.text(16, 16, 'score: 0', { fontSize: '32px', fill: '#000' });
+        
+        // Debug text
+        this.debugText = this.add.text(16, 64, 'fps: 0', { fontSize: '32px', fill: '#000' });
+
+        //  Our controls.
+        this.cursors = this.input.keyboard.createCursorKeys();
+
+
+    },
+        
+    update: function() {
+            
+            
+        //  Collide the player and the stars with the platforms
+        this.physics.arcade.collide(this.player, this.platforms);
+        this.physics.arcade.collide(this.stars, this.platforms);
+
+        //  Checks to see if the player overlaps with any of the stars, if he does call the collectStar function
+        this.physics.arcade.overlap(this.player, this.stars, this.collectStar, null, this);
+
+        //  Reset the players velocity (movement)
+        this.player.body.velocity.x = 0;
+
+        if (this.cursors.left.isDown)
+        {
+            //  Move to the left
+            this.player.body.velocity.x = -150;
+
+            this.player.animations.play('left');
+        }
+        else if (this.cursors.right.isDown)
+        {
+            //  Move to the right
+            this.player.body.velocity.x = 150;
+
+            this.player.animations.play('right');
+        }
+        else
+        {
+            //  Stand still
+            this.player.animations.stop();
+
+            this.player.frame = 4;
+        }
+
+        //  Allow the player to jump if they are touching the ground.
+        if (this.cursors.up.isDown && this.player.body.touching.down)
+        {
+            this.player.body.velocity.y = -350;
+        }
+        
+        this.debugText.text = "FPS: " + this.time.fps;  
+    },
+    
+    render: function()
+    {
+        this.debug.text(this.time.fps || '--', 2, 14, "#00ff00");   
+    },
+
+    collectStar: function (player, star) {
+
+        // Removes the star from the screen
+        star.kill();
+
+        //  Add and update the score
+        this.score += 10;
+        this.scoreText.text = 'Score: ' + this.score;
+
+    },
+
+    gameResized: function (width, height) {
+
+        // This could be handy if you need to do any extra processing if the 
+        // game resizes. A resize could happen if for example swapping 
+        // orientation on a device or resizing the browser window. Note that 
+        // this callback is only really useful if you use a ScaleMode of RESIZE 
+        // and place it inside your main game state.
+
+    },
+
+};
